12eeb9a986e5fc905e1a5ae45e4705ffc9aaa78a
Akniyet Turdybay <akniyet@Turdybays-Air.kbtu.kz>
2022-03-10 21:13:59 +0600

    feat: modal project added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import Modal from 'react-modal';
 import './App.css';
 
+
 function App() {
+
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const openModal = () => {
+    setOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal
+        isOpen={isOpen}
+      >
+
+<div className="rgb">
+        <div className="canvas">
+            <div className="well">
+                <h2>Добро пожаловать!</h2>
+            </div>
+            <div className="about">
+                Вы соглашаетесь на уведомления о наших свежих новостях?
+                <br/> Благодаря соглашению вы первыми узнаете новости!
+            </div>
+            <img src="https://img.icons8.com/ios-filled/2x/lunacy-new.png" alt="" className="img-canvas" />
+            <div className="buttons">
+                <button className="btn-1" onClick={openModal}>Соглашаюсь</button>
+                <button className="btn-2" onClick={openModal}>Нет, спасибо!</button>
+            </div>
+        </div>
+    </div>
+      </Modal>
+      <button onClick={openModal}>Open</button>
     </div>
   );
 }
